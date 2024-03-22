47d8ccf6a19db7c9bf7c5a8e651df33f4109ae9c
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-03-22 10:24:37 -0500

    Placed the API call to start the app at top of API list

--- src/preload.js
+++ src/preload.js
@@ -1,10 +1,31 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 const electronAPI = {
+  //* CALL AND RESPONSE *//
+  
+  loadLoreData(path) {
+    const response = ipcRenderer.sendSync(
+      "load:lore-data-project-directory",
+      path
+    );
+    return response;
+  },
+
+  getPathSpritesPreview(fileKey) {
+    const response = ipcRenderer.sendSync("path:sprites-preview", fileKey);
+    return response;
+  },
+
+  saveImage(filePath) {
+    const response = ipcRenderer.sendSync("save:lore-image", filePath);
+    return response;
+  },
+
   //* ONE WAY FROM MAIN *//
+
   onOpenProject: (callback) =>
     ipcRenderer.on("send:catalog-data", (_event, value) => callback(value)),
-  // make a function to handle the project directory update
+
   onSetProjectDirectory: (callback) =>
     ipcRenderer.on("send:current-directory", (_event, value) =>
       callback(value)
@@ -14,21 +35,5 @@ const electronAPI = {
   saveLore: (data) => ipcRenderer.send("save:lore-information", data),
   saveTemplates: (data) => ipcRenderer.send("save:templates-information", data),
 
-  //* CALL AND RESPONSE *//
-  getPathSpritesPreview(fileKey) {
-    const response = ipcRenderer.sendSync("path:sprites-preview", fileKey);
-    return response;
-  },
-  saveImage(filePath) {
-    const response = ipcRenderer.sendSync("save:lore-image", filePath);
-    return response;
-  },
-  loadLoreData(path) {
-    const response = ipcRenderer.sendSync(
-      "load:lore-data-project-directory",
-      path
-    );
-    return response;
-  },
 };
 contextBridge.exposeInMainWorld("electronAPI", electronAPI);
